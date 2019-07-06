90eb91ff37e3ff216f2c2b874395c41491247303
Claudia Gutierrez <gutierrez.cp21@yahoo.com>
2019-07-05 21:17:39 -0400

    models

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongojs = require("mongojs");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const router = require("./controllers/route");
+const mongoose = require("mongoose");
+const Index = require("./models/index");
 
 const app = express();
 
@@ -12,16 +14,24 @@ const collection = ["little-animals"];
 
 const db = mongojs(databaseUrl, collection);
 
+mongoose.connect("mongodb://localhost/sweetonesdb", { useNewUrlParser: true});
+
 app.use(router);
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-
-db.on("error", function(error){
-    console.log("Database Error: ", error);
+Index.create(data)
+.then(function(res){
+    console.log(res);
+})
+.catch(function(error){
+    console.log(error.message);
 });
+// db.on("error", function(error){
+//     console.log("Database Error: ", error);
+// });
 
 app.listen(3000, function() {
     console.log("App running on port 3000!");
